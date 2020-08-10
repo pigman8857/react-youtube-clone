e51c901fd16ba26bd4be49fd33343f1044676184
@somkamolv <somkamol@hot-now.com>
2020-08-10 16:18:29 +0700

    Add SearchBar, VideoList and VideoList component files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
+import { Grid } from '@material-ui/core';
+
+import {SearchBar, VideoList, VideoDetail} from './components'
+
+import youtubeApi from './api/youtube';
 
 class App extends Component{
     render(){
         return (
-            <h1>Youtube Clone app</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}> 
+                            {/*Search bar */}
+                        </Grid>
+                        <Grid item xs={8}> 
+                             {/*Video detail */}
+                        </Grid>
+                        <Grid item xs={4}> 
+                            {/*Video list */}
+                            
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         );
     }
 }
